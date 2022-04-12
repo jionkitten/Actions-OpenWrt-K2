ac6c61b8d777e6dbe986ac15e3328a0556939999
jionkitten <jionkitten@outlook.com>
2022-04-12 12:54:47 +0800

    add mi-r3g

---
+++ xwrt/diy-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/thinktip/luci-theme-neobird package/feeds/luci/luci-theme-neobird
+git clone https://github.com/tty228/luci-app-serverchan package/feeds/luci/luci-app-serverchan
+git clone https://github.com/scutclient/luci-app-scutclient package/feeds/luci/luci-app-scutclient
+git clone https://github.com/scutclient/scutclient package/scutclient
+
+# Patch luci-app-scutclient
+sed -i '14c include $(TOPDIR)/feeds/luci/luci.mk' package/feeds/luci/luci-app-scutclient/Makefile
