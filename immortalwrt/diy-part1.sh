f6c10bb0795a17176d229e60118ddd0b30641a59
jionkitten <30788097+jionkitten@users.noreply.github.com>
2022-05-28 21:12:59 +0800

    Update diy-part1.sh

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -10,9 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+git clone https://github.com/messense/aliyundrive-webdav /opt/aliyundrive
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git v2raya https://github.com/v2rayA/v2raya-openwrt' >>feeds.conf.default
+echo 'src-link aliyundrive /opt/aliyundrive/openwrt' >>feeds.conf.default
