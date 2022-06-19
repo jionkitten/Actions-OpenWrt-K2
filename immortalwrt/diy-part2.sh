986ad33e50ecf7afe13efa8f94bbeb4e810f4a4a
jionkitten <30788097+jionkitten@users.noreply.github.com>
2022-06-19 10:29:13 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -10,6 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+git clone https://github.com/thinktip/luci-theme-neobird package/feeds/luci/luci-theme-neobird
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 CUR_PWD=$(pwd)
@@ -19,6 +20,12 @@ cd $CUR_PWD/package/feeds/luci/luci-app-scutclient
 patch -p1 <scutclient.patch
 
 cd $CUR_PWD
+cp files/mosdns.patch package/feeds/mosdns/luci-app-mosdns
+cd $CUR_PWD/package/feeds/mosdns/luci-app-mosdns
+git apply mosdns.patch
 
+cd $CUR_PWD
 mkdir -p package/xray-geodata
 cp files/xray-geodata/Makefile package/xray-geodata
+
+cd $CUR_PWD
