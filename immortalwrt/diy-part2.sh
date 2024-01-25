69f60fde0eea5e671bbb37e18cbaf5c6645f49ea
jionkitten <jionkitten@outlook.com>
2024-01-25 14:49:47 +0800

    update

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -10,38 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-CUR_PWD=$(pwd)
-
-cd $CUR_PWD
-cp files/passwall2.patch package/openwrt-passwall2
-cd $CUR_PWD/package/openwrt-passwall2
-git apply passwall2.patch
-
-cd $CUR_PWD
-cp files/scutclient.patch package/feeds/luci/luci-app-scutclient/
-cd $CUR_PWD/package/feeds/luci/luci-app-scutclient
-patch -p1 <scutclient.patch
-
-cd $CUR_PWD
-cp files/mosdns.patch package/feeds/mosdns/luci-app-mosdns
-cd $CUR_PWD/package/feeds/mosdns/luci-app-mosdns
-git apply mosdns.patch
-
-cd $CUR_PWD
-cp files/zerotier.patch package/feeds/luci/luci-app-zerotier
-cd $CUR_PWD/package/feeds/luci/luci-app-zerotier
-git apply zerotier.patch
-
-cd $CUR_PWD
-cp files/mt76.patch ./
-git apply ./mt76.patch
-
-cd $CUR_PWD
-mkdir -p package/xray-geodata
-cp files/xray-geodata/Makefile package/xray-geodata
-
-cd $CUR_PWD
-rm files -rf
